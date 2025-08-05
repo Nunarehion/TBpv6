1f5b2dc992b30c5e9d727a79ae3d71ff0c94fd91
Smailjic <.email@example.com>
2025-08-05 22:34:41 +0500

    test

--- services/AdminPanel/frontend/src/routes/(constructor)/+layout.svelte
+++ services/AdminPanel/frontend/src/routes/(constructor)/+layout.svelte
@@ -1,29 +1,4 @@
-<!-- <script>
-	import DocumentPreview from '$lib/components/DocumentPreview.svelte';
+<script>
+</script>
 
-	import { onMount } from 'svelte';
-	import {
-		documents,
-		loadingDocuments,
-		saveDocument,
-		loadDocuments,
-		addDocument,
-		deleteDocument
-	} from '$lib/stores/_db.js';
-
-	onMount(() => {
-		loadDocuments(collectionName);
-	});
-
-	function handleSave(doc) {
-		saveDocument(collectionName, doc);
-	}
-
-	async function handleAdd(doc) {
-		await addDocument(collectionName, doc);
-	}
-
-	async function handleDelete(doc) {
-		await deleteDocument(collectionName, doc._id);
-	}
-</script> -->
+общий лайяут
